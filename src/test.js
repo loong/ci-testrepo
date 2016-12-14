d9ba593be1738229064eb3b42356e36eda282f91
Long Hoang <long@mindworker.de>
2016-12-14 21:01:46 +0800

    First commit with FizzBuzz and dummy TODO comments

--- src/test.js
+++ src/test.js
@@ -1,8 +1,11 @@
 'use strict'
 
+// TODO Add documentation
 function fizzbuzz(n) {
+
   for (var i=1; i <= n; i++)
   {
+    // TODO Simplify this
     if (i % 15 == 0)
       console.log("FizzBuzz");
     else if (i % 3 == 0)
